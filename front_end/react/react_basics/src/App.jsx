55c87fc22ab9fda388e1b65f34a69ed896171c10
Dineshkumar Thangavel <sendmail2dk@gmail.com>
2025-01-29 09:25:56 +0530

    React_Basics: Layout example added, Project Proposal updated

--- front_end/react/react_basics/src/App.jsx
+++ front_end/react/react_basics/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/header";
-import LeftSideBar from "./components/LeftSideBar";
-import Main from "./components/main";
-import RightSideBar from "./components/RightSideBar";
-import Footer from "./components/footer";
+import Header from "./components/layout/Header";
+import LeftSideBar from "./components/layout/LeftSideBar";
+import Main from "./components/layout/Main";
+import RightSideBar from "./components/layout/RightSideBar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
